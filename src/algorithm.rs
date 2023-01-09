69be2e49f5542f13ff16c5d08741b7bf65384306
Bernardo Lansing <bernardolans@gmail.com>
2023-01-09 11:14:00 -0300

    creating a implicant from two other implicants

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -12,12 +12,30 @@ impl<'a> GroupStructure<'a> {
         // +1 because a function with n variables can have n + 1 different amounts of true values
         (0..amount_of_variables + 1).for_each(|_| groups.push(Vec::new()));
 
-        GroupStructure { groups, amount_of_groups: amount_of_variables }
+        GroupStructure { groups, amount_of_groups: amount_of_variables + 1 }
     }
 
     fn add_implicant(&mut self, implicant: &'a Implicant) {
         let index = implicant.amount_of_true_variables();
         self.groups[index].push(implicant);
+    }
+
+    // in progress
+    fn combination_step(&self) {
+        for (index, group) in self.groups[0..self.groups.len() - 1].iter().enumerate() {
+            for implicant in group {
+                let mut already_found_combinable = false;
+
+                for candidate in &self.groups[index + 1] {
+                    let combines = implicant.check_if_combines(candidate);
+
+                    if combines {
+                        already_found_combinable = true;
+
+                    }
+                }
+            }
+        }
     }
 
     fn print_group(&self) {
@@ -27,7 +45,10 @@ impl<'a> GroupStructure<'a> {
         for group in 0..self.amount_of_groups {
             println!("{}", "-".repeat(row_length));
             print!("G{}", group);
-            self.groups[group].iter().for_each(|g| println!("\t{}", g.get_binary_representation()));
+            for implicant in &self.groups[group] {
+                let prime_mark = if implicant.is_prime() { " *".to_string() } else { String::new() };
+                println!("\t{}{}", implicant.get_binary_representation(), prime_mark);
+            }
         }
         println!("{}", "-".repeat(row_length));
     }
@@ -37,26 +58,29 @@ pub fn algorithm(table: TruthTable) {
     let mut first_implicants: Vec<Implicant> = Vec::new();
     for (index, row) in table.input_rows().iter().enumerate() {
         if table.row_value(index) {
-            first_implicants.push(Implicant::new(row, table.variables_names()));
+            first_implicants.push(Implicant::from_input(row, table.variables_names()));
         }
     }
 
     println!("Function is defined by the unoptimized expression:");
-    println!("{}", first_implicants.iter()
-        .map(|implicant| implicant.get_string_representation())
-        .collect::<Vec<String>>()
-        .join(" + ")
-    );
+    println!("{}", assemble_expression(&first_implicants));
 
     println!("\nBeggining iterative optimization by Quine-McCluskey algorithm.");
-    let amount_of_groups = table.amount_of_variables();
-    let groups = agroup(&first_implicants, amount_of_groups);
+    let amount_of_variables = table.amount_of_variables();
+    let groups = agroup(&first_implicants, amount_of_variables);
     groups.print_group();
 }
 
-fn agroup(implicants: &Vec<Implicant>, amount_of_groups: usize) -> GroupStructure {
-    let mut groups = GroupStructure::new(amount_of_groups);
+fn agroup(implicants: &Vec<Implicant>, amount_of_variables: usize) -> GroupStructure {
+    let mut groups = GroupStructure::new(amount_of_variables);
     implicants.iter().for_each(|implicant| groups.add_implicant(implicant));
 
     groups
+}
+
+fn assemble_expression(implicants: &Vec<Implicant>) -> String {
+    implicants.iter()
+        .map(|implicant| implicant.get_string_representation())
+        .collect::<Vec<String>>()
+        .join(" + ")
 }
--- src/minterm.rs
+++ src/minterm.rs
@@ -1,9 +1,19 @@
+use std::mem::discriminant;
 use crate::truth_table::TruthTable;
 
-enum LogicLoad {
-    False,
-    True,
-    DontMatter
+#[derive(Copy, Clone)]
+enum LogicLoad { False, True, DontMatter }
+
+impl LogicLoad {
+    fn is_true(&self) -> bool { match self { Self::True => true, _ => false } }
+    fn dont_matter(&self) -> bool { match self { Self::DontMatter => true, _ => false } }
+    fn is_false(&self) -> bool { match self { Self::False => true, _ => false } }
+    fn equals(&self, other: &LogicLoad) -> bool {
+        let both_true = self.is_true() && other.is_true();
+        let both_dont_matter = self.dont_matter() && other.dont_matter();
+        let both_false = self.is_false() && other.is_false();
+        both_true || both_dont_matter || both_false
+    }
 }
 
 struct MintermFragment {
@@ -32,12 +42,17 @@ impl MintermFragment {
 
 pub struct Implicant {
     variables_names: Vec<String>,
-    fragments: Vec<MintermFragment>
+    fragments: Vec<MintermFragment>,
+    marked_as_prime: bool // a priori, will be set to false. Algorithm will set it to true
+    // if later verified that it is a prime.
 }
 
 impl Implicant {
-    // this overload creates the first implicants, which are built from input rows.
-    pub fn new(row_of_inputs: &Vec<bool>, variables_names: Vec<String>) -> Self {
+    pub fn is_prime(&self) -> bool { self.marked_as_prime }
+    pub fn mark_as_prime(&mut self) { self.marked_as_prime = true }
+
+    // creates the first implicants, which are built from input rows.
+    pub fn from_input(row_of_inputs: &Vec<bool>, variables_names: Vec<String>) -> Self {
         let mut fragments = Vec::new();
 
         for (index, value) in row_of_inputs.iter().enumerate() {
@@ -51,13 +66,56 @@ impl Implicant {
             );
         }
 
-        Implicant { variables_names, fragments }
+        Implicant { variables_names, fragments, marked_as_prime: false }
+    }
+
+    pub fn from_implicants(impl_a: &Implicant, impl_b: &Implicant) -> Self {
+        let mut new_fragments = Vec::new();
+        let mut unequal_fragments_found: usize = 0;
+
+        for (frag_a, frag_b) in impl_a.fragments.iter().zip(impl_b.fragments.iter()) {
+            let variable_name = frag_a.variable_name.to_string();
+
+            if frag_a.logic_load.equals(&frag_b.logic_load) {
+                let logic_load = frag_a.logic_load;
+                new_fragments.push(MintermFragment { variable_name, logic_load });
+            }
+
+            else {
+                unequal_fragments_found += 1;
+                new_fragments.push(
+                    MintermFragment { variable_name, logic_load: LogicLoad::DontMatter }
+                );
+            }
+        }
+
+        if unequal_fragments_found != 1 { panic!("Trying to combine incompatible implicants.") }
+
+        Implicant {
+            variables_names: impl_a.variables_names.clone(),
+            fragments: new_fragments,
+            marked_as_prime: false,
+        }
     }
 
     pub fn amount_of_true_variables(&self) -> usize {
         self.fragments.iter()
             .filter(|frag| match frag.logic_load { LogicLoad::True => true, _ => false})
             .count()
+    }
+
+    pub fn check_if_combines(&self, other: &Implicant) -> bool {
+        let mut differences: usize = 0;
+        for (self_var, other_var) in self.fragments.iter().zip(other.fragments.iter()) {
+            match (&self_var.logic_load, &other_var.logic_load) {
+                (LogicLoad::True, LogicLoad::True) => {},
+                (LogicLoad::DontMatter, LogicLoad::DontMatter) => {},
+                (LogicLoad::False, LogicLoad::False) => {},
+                _ => { differences += 1; }
+            }
+        }
+
+        differences == 1
     }
 
     pub fn get_string_representation(&self) -> String {
