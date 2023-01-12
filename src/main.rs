d6624b6db7d29a15a9e92238025e1b0dbb518b8a
Bernardo Lansing <bernardolans@gmail.com>
2023-01-11 21:38:30 -0300

    receiving truth table filepath via cli arg, removed office dependency

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,18 @@ mod coverage_map;
 
 use truth_table::TruthTable;
 use algorithm::algorithm;
+use std::env;
 
 fn main() {
-    let table = TruthTable::from_csv("example_tables/func-a.csv");
+    let mut args = env::args();
+    args.next(); // discard program name arg
+    let provided_filepath = args.next();
+
+    let filepath = match provided_filepath {
+        Some(path) => path,
+        None => panic!("Please, provide a filepath to a truth table (has to be .csv).")
+    };
+    let table = TruthTable::from_csv(filepath.as_str());
 
     algorithm(table);
 }
-
-
